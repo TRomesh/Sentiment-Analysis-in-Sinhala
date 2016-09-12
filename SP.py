24fb495eb96e6f1d88717573fd9c342c59b7ad92
Tharaka Romesh <Tharaka Romesh>
2016-09-13 00:45:43 +0530

    added modified data set for scikit learn example

--- SP.py
+++ SP.py
@@ -4,9 +4,9 @@ import os
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-postrain = __location__ + "train/postive/positive.txt"
-negtrain = __location__ + "train/negitive/negitive.txt"
-strain = __location__ + "train/stopwords/stopwords.txt"
+postrain = __location__ + "\\train\\positive\\positive.txt"
+negtrain = __location__ + "\\train\\negitive\\negative.txt"
+strain = __location__ + "\\train\\stopwords\\stopwords.txt"
 
 neglist = []
 poslist = []
@@ -14,15 +14,15 @@ stlist = []
 
 
 #Load positive sentences into a list
-pw = open(postrain, 'r')
+pw = open(postrain, encoding="utf8")
 postxt = pw.readlines()
 
 #Load negative sentences into a list
-nw = open(negtrain, 'r')
+nw = open(negtrain, encoding="utf8")
 negtxt = nw.readlines()
 
 #Load stop words into a list
-sw = open(postrain, 'r')
+sw = open(postrain, encoding="utf8")
 stptxt = sw.readlines()
 
 
@@ -38,40 +38,39 @@ for i in range(0,len(negtxt)):
 for i in range(0,len(stptxt)):
     stlist.append('stop')
 
-#Creates a list of tuples, with sentiment tagged.
+#Creates a list of tuples, with sentence tagged.
 postagged = zip(postxt, poslist)
 negtagged = zip(negtxt, neglist)
 
-#Combines all of the tagged tweets to one large list.
-taggedtweets = postagged + negtagged
+#Combines all of the tagged sentence to one large list.
+taggedsentence = postagged + negtagged
 
 sentences = []
 
-#Create a list of words in the tweet, within a tuple.
-for (word, sentiment) in taggedtweets:
+#Create a list of words in the sentence, within a tuple.
+for (word, sentiment) in taggedsentence:
     word_filter = [i.lower() for i in word.split()]
     sentences.append((word_filter, sentiment))
 
-#Pull out all of the words in a list of tagged tweets, formatted in tuples.
-def getwords(tweets):
+#Pull out all of the words in a list of tagged sentence, formatted in tuples.
+def getwords(sentences):
     allwords = []
-    for (words, sentiment) in tweets:
+    for (words, sentiment) in sentences:
         allwords.extend(words)
     return allwords
 
 #Order a list of tweets by their frequency.
-def getwordfeatures(listoftweets):
+def getwordfeatures(listofsentences):
     #Print out wordfreq if you want to have a look at the individual counts of words.
-    wordfreq = nltk.FreqDist(listoftweets)
+    wordfreq = nltk.FreqDist(listofsentences)
     words = wordfreq.keys()
     return words
 
-#Calls above functions - gives us list of the words in the tweets, ordered by freq.
+#Calls above functions - gives us list of the words in the sentence, ordered by freq.
 print(getwordfeatures(getwords(sentences)))
 
 wordlist = getwordfeatures(getwords(sentences))
-"""wordlist = [i for i in wordlist if not i in stopwords.words('english')]"""
-wordlist = [i for i in wordlist if not i in customstopwords]
+wordlist = [i for i in wordlist if not i in stptxt]
 
 def feature_extractor(doc):
     docwords = set(doc)
@@ -87,11 +86,11 @@ classifier = nltk.NaiveBayesClassifier.train(training_set)
 print(classifier.show_most_informative_features(n=30))
 
 while True:
-    input = raw_input('ads')
+    input = raw_input("Enter any sentence or 'exit' to quit:")
     if input == 'exit':
         break
     elif input == 'informfeatures':
-        print classifier.show_most_informative_features(n=30)
+        print(classifier.show_most_informative_features(n=30))
         continue
     else:
         input = input.lower()
